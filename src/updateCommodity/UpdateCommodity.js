f7132eeced91e2a1048609532292916e9653781d
Bjorn <bjornwl@uio.no>
2023-10-20 12:23:13 +0200

    fetch lifesaving comodity

--- src/updateCommodity/UpdateCommodity.js
+++ src/updateCommodity/UpdateCommodity.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React from "react"
+import { useDataQuery } from '@dhis2/app-runtime'
+import {CircularLoader} from '@dhis2/ui'
 
 
 export function UpdateCommodity(props) {
+
+    const request = {
+        request0: {
+          resource: "/dataSets/ULowA8V3ucd",
+          params: {
+            fields: "displayName,dataSetElements[dataElement[id,displayName]]"
+          }
+        }
+      }
+
+    const { loading, error, data } = useDataQuery(request)
+      if (error) {
+          return <span>ERROR: {error.message}</span>
+      }
+  
+      if (loading) {
+          return <CircularLoader/>
+      }
+  
+      if (data) {
+        console.log(data.request0.dataSetElements)
+        return (
+            <h1>UpdateCommodity</h1>
+    )}
+
+    
+    
     return (
         <h1>UpdateCommodity</h1>
-    );
+    )
 }
