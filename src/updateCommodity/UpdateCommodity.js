08a179c481450810365d2ea3773b2030926cc12a
Bjorn <bjornwl@uio.no>
2023-10-21 12:40:17 +0200

    added button and improoved the dropdown menu

--- src/updateCommodity/UpdateCommodity.js
+++ src/updateCommodity/UpdateCommodity.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import { useDataQuery } from '@dhis2/app-runtime'
-import {CircularLoader,
-    DropdownButton,
-    FlyoutMenu,
-    MenuItem
+import { CircularLoader,
+    SingleSelectField ,
+    SingleSelectOption ,
+    MenuItem,
+    ReactFinalForm,
+    Field,
+    Input,
+    Button 
 } from '@dhis2/ui'
 
 
 export function UpdateCommodity(props) {
+
+    const [selectedCommodity, setSelectedCommodity] = useState()
+    const [selectedAmount, setSelectedAmount] = useState()
 
     const request = {
         request0: {
@@ -18,6 +25,22 @@ export function UpdateCommodity(props) {
         }
       }
 
+    function submit () {
+        if (!(selectedCommodity)) {
+            alert("No commodity was selected")
+            return
+        }
+        if (!(selectedAmount)) {
+            alert("No number provided")
+            return
+        }
+        if (selectedAmount < 0 || selectedAmount % 1 != 0) {
+            alert("innvalid number " + selectedAmount + "\nThe number must be a whole number greater than 0")
+            return
+        }
+        console.log(selectedAmount)
+    }
+
     const { loading, error, data } = useDataQuery(request)
       if (error) {
           return <span>ERROR: {error.message}</span>
@@ -26,22 +49,40 @@ export function UpdateCommodity(props) {
       if (loading) {
           return <CircularLoader/>
       }
-  
       if (data) {
-        console.log(data.request0.dataSetElements[0].dataElement.displayName)
         return (
-            <DropdownButton
-            component={<FlyoutMenu>
-                {data.request0.dataSetElements.map(elem => {
-                    const names = elem.dataElement.displayName.split("- ")[1]
-                    return(
-                        <MenuItem key={names} label={names} id={names}></MenuItem>
-                    )
-                })}
-                </FlyoutMenu>}
-            name="buttonName"
-            value="buttonValue"
-            >{data.request0.displayName}</DropdownButton>
+            <ReactFinalForm.Form onSubmit={submit}>
+                {({ handleSubmit }) => (
+                    <form onSubmit={handleSubmit}>
+                        <SingleSelectField
+                            label={data.request0.displayName}
+                            onChange={(data) => setSelectedCommodity(data.selected)}
+                            selected={selectedCommodity}
+                            id="singleSelect"
+                            placeholder="Select - One"
+                        >
+                            {data.request0.dataSetElements.map(elem => {
+                                const names = elem.dataElement.displayName.split("- ")[1]
+                                return(
+                                    <SingleSelectOption key={names} value={names} label={names}></SingleSelectOption>
+                                )
+                            })}
+                        </SingleSelectField>
+                        <Field
+                            label="new amount"
+                        >
+                            <Input
+                                onChange={(data) => setSelectedAmount(data.value)}
+                                type="number"
+                                step="1"
+                                min="0"
+                                id="uppdateNum"
+                            ></Input>
+                        </Field>
+                        <Button type="submit">Submit</Button>
+                    </form>
+                )}
+            </ReactFinalForm.Form> 
     )}
     
     return (
