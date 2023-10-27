a2249fbff81d604ee125157b9834df51aaa9ac32
Una Mathiesen Langeland <unla@itu.dk>
2023-10-27 10:20:05 +0200

    gitignore

--- .d2/shell/src/D2App/dispenseCommodity/DispenseCommodity.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export function DispenseCommodity(props) {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "DispenseCommodity"));
-}
