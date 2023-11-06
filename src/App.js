5bca516268883adc60a06997a4ea1c0c26898f2a
Bjorn <bjornwl@uio.no>
2023-11-06 22:12:16 +0100

    fetch and push values throug props

--- src/App.js
+++ src/App.js
@@ -8,30 +8,39 @@ import { DispenseCommodity } from "./dispenseCommodity/DispenseCommodity";
 import { UpdateCommodity } from "./updateCommodity/UpdateCommodity";
 import { StoreManagement } from "./storeManagement/StoreManagement";
 import { useState } from "react";
+import { getCommoditiesData } from "./ApiCalls";
 
 function MyApp() {
   const [activePage, setActivePage] = useState("Browse");
   function activePageHandler(page) {
     setActivePage(page);
   }
+  // get commdities and their values
+  // these are passed through props
+  const [data, refetch] = getCommoditiesData()
 
-  return (
-    <div className={classes.container}>
-      <div className={classes.left}>
-        <Navigation
-          activePage={activePage}
-          activePageHandler={activePageHandler}
-        />
+  if (!(data instanceof Array)){
+    return data
+  }
+  else {
+    return (
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <Navigation
+            activePage={activePage}
+            activePageHandler={activePageHandler}
+          />
+        </div>
+        <div className={classes.right}>
+          {activePage === "CommodityTable" && <CommodityTable data={data} refetch={refetch}/>}
+          {activePage === "DispenseCommodity" && <DispenseCommodity data={data} refetch={refetch}/>}
+          {activePage === "RequestCommodity" && <RequestCommodity data={data} refetch={refetch}/>}
+          {activePage === "UpdateCommodity" && <UpdateCommodity data={data} refetch={refetch}/>}
+          {activePage === "StoreManagement" && <StoreManagement data={data} refetch={refetch}/>}
+        </div>
       </div>
-      <div className={classes.right}>
-        {activePage === "CommodityTable" && <CommodityTable />}
-        {activePage === "DispenseCommodity" && <DispenseCommodity />}
-        {activePage === "RequestCommodity" && <RequestCommodity />}
-        {activePage === "UpdateCommodity" && <UpdateCommodity />}
-        {activePage === "StoreManagement" && <StoreManagement />}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default MyApp;
