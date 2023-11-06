647ffbbea43fd1556144747a6d86a511e86e8a33
Bjorn <bjornwl@uio.no>
2023-11-06 22:10:49 +0100

    added documentation and made the data refetchable

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -5,17 +5,28 @@ import { requestCommodities, dataMutationQuery, requestCommodityValues, requestC
 
 const getCommoditiesNames = () => {
     /*
-    gets the names and id of commodities
+    fetches the names and id of commodities
+
+    each element is a map with these values:
+        displayName: the name of the commodity
+        id: the id of the commodity
+
+    returns:
+        list:
+            ret: one of three options:
+                loading: a circular loader
+                error: an error message
+                data: the result of the fetch represented as an array of maps
+            refetch: a callable function that refetches the data
     */
-    const { loading, error, data } = useDataQuery(requestCommodities);
-    // on error fetching commodities
-    if (error) {return <span>ERROR: {error.message}</span>}
-    // when loding fetching commodities
-    if (loading) {return <CircularLoader />}
-    // when data is pressent fetching commodities
+    const { loading, error, data, refetch } = useDataQuery(requestCommodities);
+    let ret
+    // set the result
+    if (error) {ret = <span>ERROR: {error.message}</span>}
+    if (loading) {ret = <CircularLoader />}
     if (data) {
         // ekstract elems and remove prefix of name and sort
-        return data.commodities.dataSetElements.map((elem) => {
+        ret =  data.commodities.dataSetElements.map((elem) => {
             const names = elem.dataElement.displayName.split("- ")[1];
             return { "displayName": names, "id": elem.dataElement.id }
         }).sort(function(a, b){
@@ -24,93 +35,192 @@ const getCommoditiesNames = () => {
             return 0
         })
     }
+    return [ret, refetch]
 }
 
 const getCommoditiesValues = (period=null, orgUnit="xQIU41mR69s") => {
     /*
-    gets the values and id of commodities
+    fetches the values of the commodities with it's coresponding id
+
+    each element is a map with these values:
+        attributeOptionCombo: the comodities attribute option
+        categoryOptionCombo: the comodities category option
+        comment: a comment left on last post
+        created: when the dataelement was created
+        dataElement: the id of the commodity
+        followup: a boolean value representing if a followup is intended
+        lastUpdated: last time the commodities value was uppdated
+        orgUnit: the organisations id
+        period: the period
+        storedBy: who stored the commodity
+        value: the value of the commodity
+
+    args:
+        period (default: null): the period in which to fetch the values
+        orgUnit (default: "xQIU41mR69s"): the org unit in which to fetch he values for
+
+    returns:
+        list:
+            ret: one of three options:
+                loading: a circular loader
+                error: an error message
+                data: the result of the fetch represented as an array of maps
+            refetch: a callable function that refetches the data
     */
+    // get current period if none were provided
     if (!period) {
         const date = new Date().toISOString().split("-");
         period = date[0] + date[1]
     }
-    const { loading, error, data } = useDataQuery(requestCommodityValues, {variables : {period: period, orgUnit: orgUnit}});
-    if (error) {return <span>ERROR: {error.message}</span>}
-    // when loding fetching commodities
-    if (loading) {return <CircularLoader />}
-    // when data is pressent fetching commodities
-    if (data) {
-        return data.comVals.dataValues
-    }
+    const { loading, error, data, refetch } = useDataQuery(requestCommodityValues, {variables : {period: period, orgUnit: orgUnit}});
+    let ret
+    // set the result
+    if (error) {ret = <span>ERROR: {error.message}</span>}
+    if (loading) {ret = <CircularLoader />}
+    if (data) {ret = data.comVals.dataValues}
+    return [ret, refetch]
 }
 
 const getCategoryOptionCombos = () => {
-    const { loading, error, data } = useDataQuery(requestCategoryOptionCombos);
-    if (error) {return <span>ERROR: {error.message}</span>}
-    // when loding fetching commodities
-    if (loading) {return <CircularLoader />}
-    // when data is pressent fetching commodities
-    if (data) {
-        console.log(data.comVals.categoryOptionCombos)
-        return data.comVals.categoryOptionCombos
-    }
+    /*
+    fetch all the different category options combos with their display name
+
+    each element is a map with these values:
+        id: the id of the category option combo
+        displayName: the display name category option combo
+
+    returns:
+        list:
+            ret: one of three options:
+                loading: a circular loader
+                error: an error message
+                data: the result of the fetch represented as an array of maps
+            refetch: a callable function that refetches the data
+    */
+    const { loading, error, data, refetch } = useDataQuery(requestCategoryOptionCombos);
+    let ret
+    // set the result 
+    if (error) {ret = <span>ERROR: {error.message}</span>}
+    if (loading) {ret = <CircularLoader />}
+    if (data) {ret = data.comVals.categoryOptionCombos}
+    return [ret, refetch]
 }
 
 const getCommoditiesData = (period=null, orgUnit="xQIU41mR69s") => {
     /*
-    gets the names, id, values, catOptCom, catOptComName of commodities
+    fetch the commodities with displayname for them and their category option combo
+
+    each element is a map with these values:
+        DataElementName: the name of the commodity
+        DataElement: the id of the commodity
+        value: the value of the commodity
+        categoryOptionCombos: the category option combo id of the commodity
+        categoryOptionCombosName:the category option combo display name of the commodity
+
+    args:
+        period (default: null): the period in which to fetch the values
+        orgUnit (default: "xQIU41mR69s"): the org unit in which to fetch he values for
+
+    returns:
+        list:
+            ret: one of three options:
+                loading: a circular loader
+                error: an error message
+                data: the result of the fetch represented as an array of maps
+            refetch: a callable function that refetches the data
     */
+    // get all values from functions fething them
     const coms = getCommoditiesNames()
-    const vals = getCommoditiesValues()
+    const vals = getCommoditiesValues(period, orgUnit)
     const catOpt = getCategoryOptionCombos()
-    console.log("test")
-    if (!(coms instanceof Array)){
-        return coms
+    // function to refetch all the values
+    const refetch = () => {
+        coms[1]()
+        vals[1]()
+        catOpt[1]()
     }
-    if (!(catOpt instanceof Array)){
-        return catOpt
+    // make the value to be returned the data of one of them if anny are loading or there is an error
+    let ret
+    if (!(coms[0] instanceof Array)){
+        ret = coms[0]
     }
-    if (!(vals instanceof Array)){
-        return vals
+    else if (!(catOpt[0] instanceof Array)){
+        ret = catOpt[0]
     }
-    return vals.map((elem) => {
-        return { 
-            "DataElementName": coms.filter((x) => {return x.id === elem.dataElement})[0].displayName, 
-            "DataElement": elem.dataElement, 
-            "value": elem.value, 
-            "categoryOptionCombos": elem.categoryOptionCombo,
-            "categoryOptionCombosName": catOpt.filter((x) => {return x.id === elem.categoryOptionCombo})[0].displayName
-        }
-    }).sort(function(a, b){
-        if (a.DataElementName > b.DataElementName) {return 1}
-        if (a.DataElementName < b.DataElementName) {return -1}
-        return 0
-    })
+    else if (!(vals[0] instanceof Array)){
+        ret = vals[0]
+    }
+    // if all data is pressent map them together to a useful format
+    // then sort it twice, first on name then on category combo
+    else {
+        ret = vals[0].map((elem) => {
+            return { 
+                "DataElementName": coms[0].filter((x) => {return x.id === elem.dataElement})[0].displayName, 
+                "DataElement": elem.dataElement, 
+                "value": elem.value, 
+                "categoryOptionCombos": elem.categoryOptionCombo,
+                "categoryOptionCombosName": catOpt[0].filter((x) => {return x.id === elem.categoryOptionCombo})[0].displayName
+            }
+        }).sort(function(a, b){
+            if (a.DataElementName > b.DataElementName) {return 1}
+            if (a.DataElementName < b.DataElementName) {return -1}
+            return 0
+        }).sort(function(a, b){
+            if (a.categoryOptionCombos > b.categoryOptionCombos) {return 1}
+            if (a.categoryOptionCombos < b.categoryOptionCombos) {return -1}
+            return 0
+        })
+    }
+    return [ret, refetch]
 }
 
 const changeCommodityCountMutator = () => {
     /*
     use data mutation hook
-    returns the mutator
+    returns the mutator and its query confimation (data)
+
+    the returned value is intended to be passed directly as mutator to the changeCommodityCount function
+
+    returns:
+        list:
+            mutate: a callable function which mutates the post querry
+            data: query confimation
     */
-    const [mutate, { loadingM, errorM }] = useDataMutation(dataMutationQuery);
-    return mutate
+    const [mutate, { loading, error, data }] = useDataMutation(dataMutationQuery);
+    return [mutate, data]
 }
 
-const changeCommodityCount = (mutator, value, dataElement, period = null, orgUnit = "xQIU41mR69s") => {
+const changeCommodityCount = (mutator, value, dataElement, refetch = null, period = null, orgUnit = "xQIU41mR69s", categoryOptionCombo = "HllvX50cXC0") => {
     /*
     uses a mutator to do a post query
+
+    args:
+        mutator: the muatator data combo from the corresponding function
+        value: the value to be changed
+        dataElement: the id of the commodity to be changed
+        refetch (default: null): a refetch function which gets called after the post
+        period (default: null): a perdiod, current period will be used if not provided
+        orgUnit (default: "xQIU41mR69s"): org unit posting
+        categoryOptionCombo (default "HllvX50cXC0"): category option combo, the default id is for "default" catoegory option
     */
+    // get current period if not provided
     if (!period) {
         const date = new Date().toISOString().split("-");
         period = date[0] + date[1]
     }
-    mutator({
+    // do muattion
+    mutator[0]({
         value: value,
         dataElement: dataElement,
         period: period,
         orgUnit: orgUnit,
+        categoryOptionCombo: categoryOptionCombo,
     });
+    // reftech after post if provided
+    if (mutator[1] && !(refetch === null)) {
+        refetch()
+    }
 }
 
-export { changeCommodityCount, getCommoditiesNames, changeCommodityCountMutator, getCommoditiesValues, getCategoryOptionCombos, getCommoditiesData }
+// export functions so they ar accesible elsewere 
+export { changeCommodityCount, changeCommodityCountMutator, getCommoditiesData }
--- src/apiConstants.js
+++ src/apiConstants.js
@@ -6,6 +6,25 @@ export const requestCommodities = {
     },
   },
 };
+
+export const dataMutationQuery = {
+  resource: "dataValueSets",
+  type: "create",
+  dataSet: "ULowA8V3ucd",
+  data: ({ value, dataElement, period, orgUnit, categoryOptionCombo }) => ({
+    dataValues: [
+      {
+        dataElement: dataElement,
+        period: period,
+        orgUnit: orgUnit,
+        value: value,
+        categoryOptionCombo: categoryOptionCombo
+      },
+    ],
+  }),
+};
+
+// temp
 export const requestComValues = {
   request0: {
     resource: "/dataValueSets",
@@ -16,19 +35,23 @@ export const requestComValues = {
     },
   },
 };
-// Mutation query for updating data values of commodities
-export const dataMutationQuery = {
-  resource: "dataValueSets",
-  type: "create",
-  dataSet: "ULowA8V3ucd",
-  data: ({ value, dataElement, period, orgUnit }) => ({
-    dataValues: [
-      {
-        dataElement: dataElement,
-        period: period,
-        orgUnit: orgUnit,
-        value: value,
-      },
-    ],
-  }),
+
+export const requestCommodityValues = {
+  comVals: {
+    resource: "/dataValueSets",
+    params: ({ period, orgUnit }) => ({
+      orgUnit: orgUnit,
+      period: period,
+      dataSet: "ULowA8V3ucd",
+    }),
+  },
+};
+
+export const requestCategoryOptionCombos = {
+  comVals: {
+    resource: "/categoryOptionCombos",
+    params: {
+      paging: false
+    },
+  },
 };
