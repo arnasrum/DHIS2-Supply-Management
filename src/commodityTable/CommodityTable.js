617699814f74a75e23b38534b4736621d00537b7
Dawid Jakub Szefler <dawidjs@ulrik.uio.no>
2023-11-10 10:18:10 +0100

    Logics for viewing reviewed

--- src/commodityTable/CommodityTable.js
+++ src/commodityTable/CommodityTable.js
@@ -20,41 +20,79 @@ const requestCommodities = {
     }
   }
 
+const requestCategoryOptions = {
+    request0: {
+        resource: "/categoryOptionCombos",
+        params: {
+            paging: false
+        }
+    }
+}
+
 const requestValues = {
     request0: {
         resource: "/dataValueSets",
         params: {
             orgUnit: "xQIU41mR69s",
-            period: "202310",
+            period: "202311",
             dataSet: "ULowA8V3ucd"
         }
     }
 }
 
 export function CommodityTable(props) {
-    const { loading: loadingVal, error: errorVal, data: dataVal } = useDataQuery(requestValues);
     const { loading: loadingCom, error: errorCom, data: dataCom } = useDataQuery(requestCommodities);
+    const { loading: loadingCat, error: errorCat, data: dataCat } = useDataQuery(requestCategoryOptions);
+    const { loading: loadingVal, error: errorVal, data: dataVal } = useDataQuery(requestValues);
 
-    const values = new Map();
+    // data[id] = [displayName, [catOptID_1, Val_1], [...2], ...]
+    // Gets all commodieties and maps theirs ID to Displaynames in data
+    const data = new Map();
+    if (dataCom) dataCom.request0.dataSetElements.forEach((el) => {
+        data.set(el.dataElement.id, [el.dataElement.displayName]);
+    })
+
+    // Gets all commodieties' values (all kind of) and pushes to commodieties' array in data
+    const categories = new Map();
     if (dataVal) dataVal.request0.dataValues.forEach((el) => {
-        // Fetch the first element as it is the newest in the list
-        if (!values.has(el.dataElement)) values.set(el.dataElement, parseInt(el.value))
+        if (!categories.has(el.categoryOptionCombo)) {
+            dataCat.request0.categoryOptionCombos.forEach((cat) => {
+                if (cat.id === el.categoryOptionCombo) categories.set(cat.id, cat.displayName)
+            })
+        }
+        const list = data.get(el.dataElement)
+        if (list) list.push([el.categoryOptionCombo, el.value])
     });
 
+    // Creates an array of what to be displayed in the table
+    const displayData = [];
+    data.forEach((el) => {
+        const tokens = el[0].split(" ").slice(2)
+        el.slice(1).forEach((cat) => {
+            displayData.push([tokens.join(" "), categories.get(cat[0]), cat[1]])
+        })
+    });
+
+    // Sorts first by value type, then by commodity
+    displayData.sort()
+        
     if (loadingCom || loadingVal) {
         return <CircularLoader />;
     }
 
     function CommodityRows() {
-        if (dataCom) {
-            return dataCom.request0.dataSetElements.map((el) => {
-                const tokens = el.dataElement.displayName.split(" ").slice(2)
-                return (
+        if (data) {
+            const rows = [];
+            displayData.forEach((el) => {
+                rows.push(
                     <TableRow>
-                        <TableCell>{tokens.join(" ")}</TableCell>
-                        <TableCell>{values.get(el.dataElement.id)}</TableCell>
+                        <TableCell>{el[0]}</TableCell>
+                        <TableCell>{el[1]}</TableCell>
+                        <TableCell>{el[2]}</TableCell>
                     </TableRow>
-            )});
+                )
+            });
+            return rows;
         }
     }
 
@@ -65,7 +103,9 @@ export function CommodityTable(props) {
                 <TableHead>
                     <TableRowHead>
                         <TableCellHead>Commodity</TableCellHead>
-                        <TableCellHead>Stock balance</TableCellHead>
+                        <TableCellHead>Consumption</TableCellHead>
+                        <TableCellHead>End Balance</TableCellHead>
+                        <TableCellHead>Quantity to be ordered</TableCellHead>
                     </TableRowHead>
                 </TableHead>
                 <TableBody>
