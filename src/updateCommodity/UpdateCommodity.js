105418f9bebda7adafba345fd91eeb469c0da17a
Bjorn <bjornwl@uio.no>
2023-11-14 20:51:42 +0100

    add bulk opperation for recount and made mutation able to refetch

--- src/updateCommodity/UpdateCommodity.js
+++ src/updateCommodity/UpdateCommodity.js
@@ -1,35 +1,59 @@
 import React, { useState } from 'react';
 import {
-  ReactFinalForm,
-  SingleSelectFieldFF,
-  InputFieldFF,
-  composeValidators,
-  hasValue,
-  integer,
-  createMinNumber,
-  Button,
   AlertBar,
   AlertStack
 } from "@dhis2/ui";
-import { changeCommodityCount, getSingleChangeMutator } from "../logicLayer/ApiMuatations";
-import { getCommoditiesNames } from '../logicLayer/ApiCalls';
+import { changeCommodityCountMultiple, getMultipleChangeMutator } from "../logicLayer/ApiMuatations";
+import { getCommoditiesData } from '../logicLayer/ApiCalls';
+import { InputTable } from "../components/InputTable"
+import { makeDatavalueMap } from '../logicLayer/Helpers';
 
 export function UpdateCommodity(props) {
+  // states values
   const [alert, setAlert] = useState([])
-  const mutator = getSingleChangeMutator()
-  const coms = getCommoditiesNames()
+  const mutator = getMultipleChangeMutator()
+  const [coms, refetch] = getCommoditiesData()
+  const [keyCount, setKeyCount] = useState(0)
+
 
   // on submit
-  async function submit(formInput) {
-    changeCommodityCount(mutator, formInput.value, formInput.dataElement, "rQLFnNXXIL0")
-    // set alert
+  function submit(formInput) {
+    // update counter for key
+    setKeyCount(keyCount + 1)
+    // if no value updated
+    if (Object.entries(formInput).length < 1) {
+      // set alert for missing info
+      setAlert((prev) => {
+        return [...prev, (
+          <AlertBar 
+            info 
+            key={keyCount}
+            children={"no value was provided"}
+          />
+        )]
+      })
+      return
+    }
+    // mutate when value provided
+    changeCommodityCountMultiple(mutator, Object.entries(formInput).map((pair) => {
+      return makeDatavalueMap(
+        pair[1],
+        pair[0],
+        undefined,
+        "rQLFnNXXIL0"
+      )
+    }), refetch)
+    // set alert for success
     setAlert((prev) => {
       return [...prev, (
         <AlertBar 
           success 
-        >
-          {coms.filter((x) => {return x.id === formInput.dataElement})[0].displayName} successfully recounted
-        </AlertBar>
+          key={keyCount}
+          children={coms.reduce((tot, curVal) => {
+            if(Object.keys(formInput).includes(curVal.DataElement)) { return tot + ", " + curVal.DataElementName } 
+            return tot
+          }, "").slice(2) + "successfully recounted"}
+        />
       )]
     })
   }
@@ -39,41 +63,17 @@ export function UpdateCommodity(props) {
   else {
     return (
       <div>
-        <ReactFinalForm.Form onSubmit={submit}>
-        {({ handleSubmit, form }) => (
-          <form onSubmit={(event) => {
-            handleSubmit(event).then(() => form.reset())
-          }}>
-            <ReactFinalForm.Field
-              name="dataElement"
-              label="Life-Saving Commodities"
-              id="singleSelect"
-              placeholder="Select - One"
-              validate={composeValidators(hasValue)}
-              component={SingleSelectFieldFF}
-              options={coms.map((elem) => {return { label: elem.displayName, value: elem.id }})}
-              inputWidth="300px"
-            />
-            <ReactFinalForm.Field
-              name="value"
-              label="New amount"
-              placeholder="A number greater than 0"
-              inputWidth="300px"
-              component={InputFieldFF}
-
-              validate={composeValidators(
-                hasValue,
-                integer,
-                createMinNumber(0)
-              )}
-            />
-            <br />
-            <Button type="submit" primary>
-              Submit
-            </Button>
-          </form>
-        )}
-        </ReactFinalForm.Form>
+        <h1>Stock Recount</h1>
+        <InputTable
+          headerNames={["Commodity", "Current Balance", "Recounted Balance"]}
+          propertyNames={["name", "bal"]}
+          onSubmit={submit}
+          data={coms.map((item) => {
+            return {"name": item.DataElementName,
+            "id": item.DataElement,
+            "bal": item.EndBalance}
+          })}
+        />
         <AlertStack>
           {alert}
         </AlertStack>
