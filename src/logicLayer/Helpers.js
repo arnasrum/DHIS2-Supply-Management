b481fc9db1cc4ef8077f1c889b036779b22e1faa
Bjorn <bjornwl@uio.no>
2023-11-15 15:28:47 +0100

    fixed consume not refetching

--- src/logicLayer/Helpers.js
+++ src/logicLayer/Helpers.js
@@ -7,7 +7,8 @@ const consumeCommodityCount = (
     prevConsumtion,
     dataElement, 
     period = getCurPeriod(), 
-    orgUnit = "xQIU41mR69s"
+    orgUnit = "xQIU41mR69s",
+    refetch = null
     ) => {
     /**
      * for when a commodity gets dispensed
@@ -21,12 +22,14 @@ const consumeCommodityCount = (
      *     prevConsumption: the total previusely consumed
      *     dataElement: the id of the commodity to be consumed
      *     period (default: curentPeriod): a period
-     *     orgUnit (default: "xQIU41mR69s"): org unit id
+     *     orgUnit (default: "xQIU41mR69s"): org unit id'
+     *     refetch (default: null): a function used to reftch, no refetching is done if not provided
      */
     changeCommodityCountMultiple(mutator, [
         makeDatavalueMap(parseInt(prevConsumtion) + parseInt(addValue), dataElement, period, "J2Qf1jtZuj8", orgUnit),
         makeDatavalueMap(parseInt(prevValue) - parseInt(addValue), dataElement, period, "rQLFnNXXIL0", orgUnit)
-    ])
+    ],
+    refetch)
 }
 
 const getCurPeriod = () => {
