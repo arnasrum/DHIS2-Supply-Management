edce1ba4c07ba4b34c81e75e445c7502c7489fe4
Dawid Jakub Szefler <dawidjs@ulrik.uio.no>
2023-11-15 16:28:10 +0100

    Added order by functionality for com. props.

--- src/commodityTable/CommodityStock.js
+++ src/commodityTable/CommodityStock.js
@@ -6,16 +6,18 @@ import {
     TableCellHead,
     TableHead,
     TableRow,
-    TableRowHead
+    TableRowHead,
+    Tooltip
   } from "@dhis2/ui";
 import {
     getCommoditiesData
 } from "../logicLayer/ApiCalls.js";
+import "./CommodityStock.css";
 
 
 export function CommodityStock(props) {
     // [a, b]: a - which column, b - is either 0 or 1, 0 for ascending order, 1 for descending
-    const [orderBy, setOrderBy] = useState([1, 0]); 
+    const [orderBy, setOrderBy] = useState([0, 0]); 
 
     const data = getCommoditiesData();
     
@@ -52,24 +54,60 @@ export function CommodityStock(props) {
                     else return flipOrder(-1)
                 }
                 else {
-                    if (parseInt(el_a) > parseInt(el_b)) return flipOrder(1)
-                    else return flipOrder(-1)
+                    if (parseInt(el_a) > parseInt(el_b)) return flipOrder(-1)
+                    else return flipOrder(1)
                 }
             })
             return rows;
         }
     }
 
+    function changeOrder(column) {
+        if (column == orderBy[0]) setOrderBy([column, orderBy[1] ? !1 : 1 ]) // second element is basically an XOR
+        else setOrderBy([column, 0])
+    }
+
+    function Arrow(column) {
+        if (orderBy[0] != column) return
+        if (orderBy[1]) return <>↑</>
+        else return <>↓</>
+    }
+
     return (
         <>
             <h1>Commodity Stock</h1>
+            <p><strong>Hint:</strong> Click on a column name to change the order with respect to the selected property.</p>
             <Table>
                 <TableHead>
                     <TableRowHead>
-                        <TableCellHead>Commodity</TableCellHead>
-                        <TableCellHead>Consumption</TableCellHead>
-                        <TableCellHead>End Balance</TableCellHead>
-                        <TableCellHead>Quantity to be ordered</TableCellHead>
+                        <TableCellHead>
+                            <p className="cellHeadText" onClick={() => changeOrder(0)}>
+                                <Tooltip content="Name of the commodity" openDelay={500}>
+                                    Commodity {Arrow(0)}
+                                </Tooltip>
+                            </p>
+                        </TableCellHead>
+                        <TableCellHead>
+                            <p className="cellHeadText" onClick={() => changeOrder(1)}>
+                                <Tooltip content="Amount of commodities dispensed so far this period (month)" openDelay={500}>
+                                    Consumption {Arrow(1)}
+                                </Tooltip>
+                            </p>
+                        </TableCellHead>
+                        <TableCellHead>
+                            <p className="cellHeadText" onClick={() => changeOrder(2)}>
+                                <Tooltip content="Stock balance of the commodity" openDelay={500}>
+                                    End Balance {Arrow(2)}
+                                </Tooltip>
+                            </p>
+                        </TableCellHead>
+                        <TableCellHead>
+                            <p className="cellHeadText" onClick={() => changeOrder(3)}>
+                               <Tooltip content="Amount of commodities to be ordered for the next period (month)" openDelay={500}>
+                                    Quantity to be ordered {Arrow(3)}
+                                </Tooltip>
+                            </p>
+                        </TableCellHead>
                     </TableRowHead>
                 </TableHead>
                 <TableBody>
