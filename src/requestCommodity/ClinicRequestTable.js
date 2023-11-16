dc99473c62d22fb3a7c13cc95666de13ca171f79
Bjorn <bjornwl@uio.no>
2023-11-16 16:49:30 +0100

    request commodity cleanup 1

--- src/requestCommodity/ClinicRequestTable.js
+++ src/requestCommodity/ClinicRequestTable.js
@@ -3,135 +3,93 @@ import { CircularLoader, AlertBar, AlertStack } from "@dhis2/ui";
 
 import { InputTable } from "../components/InputTable";
 import { getCurPeriod } from "../logicLayer/Helpers";
-
-function getCommodityValue(dataElements, commodityID) {
-  const value = dataElements.filter((item) => {
-    if (item.dataElement == commodityID) return true;
-    return false;
-  });
-  return value[0].value;
-}
+import { log } from "../logicLayer/Log";
 
 export function ClinicRequestTable(props) {
-  const commodity = props.commodity;
-  const orgs = props.orgs;
-  const [orgData, setOrgData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [alerts, setAlerts] = useState([]);
+    const commodity = props.commodity;
+    const orgs = props.orgs;
+    const [orgData, setOrgData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [alerts, setAlerts] = useState([]);
 
-  useEffect(() => {
-    if (!commodity) {
-      return;
+    useEffect(() => {
+        if (!commodity) {
+            return;
+        }
+        setLoading(true);
+        const fetchPromises = orgs.map(async (item) => {
+            let query = "http://localhost:9999/api/dataValues.json?";
+            query = query + "de=" + commodity;
+            query = query + "&pe=" + getCurPeriod();
+            query = query + "&ou=" + item.id;
+            query = query + "&co=" + "rQLFnNXXIL0";
+            return fetch(query)
+                .then((response) => response.json())
+                .then((response) => {
+                    return {
+                        "dataElementName": item.name,
+                        "DataElement": item.id,
+                        "value": response[0]
+                    };
+                })
+        });
+
+        // Wait for all fetch requests to complete
+        Promise.all(fetchPromises)
+            .then((data) => {
+                setOrgData(data); 
+                setLoading(false)
+            })
+            .catch((error) => {
+                setAlerts((prev) =>  [...prev, <AlertBar critical children={error.toString()} key={crypto.randomUUID()}/>]);
+                console.error("Error fetching data:", error);
+            });
+        }, [commodity]);
+
+    if(loading) {
+        return <CircularLoader/>
     }
-    setLoading(true);
-    const fetchPromises = orgs.map(async (item) => {
-      let query = "http://localhost:9999/api/dataValueSets.json?";
-      query = query + "orgUnit=" + item.id;
-      query = query + "&dataSet=" + "ULowA8V3ucd";
-      query = query + "&period=" + getCurPeriod();
+    if(orgData.length > 0) {
+        return (
+        <>
+            <InputTable 
+                headerNames={["Clinic", "Value", "Request Amount"]}
+                propertyNames={["DataElement", "value"]}
+                onSubmit={onSubmit}
+                data={orgData}
+            />
+            <AlertStack>{alerts}</AlertStack>
+        </>);
+    }
+    return <h2>Please Select a Commodity</h2>
 
-      return fetch(query)
-        .then((response) => response.json())
-        .then((response) =>
-          response.dataValues.filter((item) => {
-            if (item.categoryOptionCombo == "J2Qf1jtZuj8") return true;
-            return false;
-          })
-        )
-        .then((response) => {
-          const obj = {};
-          obj.DataElement = item.id;
-          obj.DataElementName = item.name;
-          obj.DataElements = response;
-          return obj;
-        });
-    });
-
-    // Wait for all fetch requests to complete
-    Promise.all(fetchPromises)
-      .then((data) => {
-        return data;
-      })
-      .then((data) => {
-        data.forEach((item) => {
-          item["value"] = getCommodityValue(
-            item.DataElements,
-            commodity.split("&")[0]
-          );
-        });
-        setOrgData(data);
-        return data;
-      })
-      .then(() => setLoading(false))
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, [commodity]);
-
-  if (loading) {
-    return <CircularLoader />;
-  }
-  if (orgData.length > 0) {
-    console.log(orgData);
-    return (
-      <>
-        <InputTable
-          headerNames={["Clinic", "Value", "Request Amount"]}
-          propertyNames={["DataElementName", "value"]}
-          onSubmit={onSubmit}
-          data={orgData}
-        />
-        <AlertStack>{alerts.map((item) => item)}</AlertStack>
-      </>
-    );
-  }
-  return <h2>Please Select a Commodity</h2>;
-
-  function onSubmit(formInput) {
-    const date = new Date();
-    const postQuery = "http://localhost:9999/api/dataStore/IN5320-27-requests/";
-    Object.keys(formInput).forEach((org) => {
-      const clinicName = orgs.filter((item) => item.id == org)[0].name;
-      console.log(clinicName);
-      fetch(postQuery + crypto.randomUUID(), {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({
-          commodityID: commodity.split("&")[0],
-          commodityName: commodity.split("&")[1],
-          requestedClinic: org,
-          date: date.toISOString(),
-          requestedAmount: formInput[org],
-          requestBy: props.usersOrg,
-          requestedClinicName: clinicName,
-        }),
-      })
-        .then((response) => response.json())
-        .then((response) => console.log(response))
-        .then(() =>
-          setAlerts((prevState) => [
-            ...prevState,
-            <AlertBar success key={crypto.randomUUID()}>
-              {"Requested " +
-                clinicName +
-                " " +
-                formInput[org] +
-                " " +
-                commodity.split("&")[1]}
-            </AlertBar>,
-          ])
-        )
-        .catch((error) => {
-          setAlerts((prevState) => [
-            ...prevState,
-            <AlertBar critical key={crypto.randomUUID()}>
-              {error.message}
-            </AlertBar>,
-          ]);
-          console.error(error);
-        });
-    });
-  }
+    function onSubmit(formInput) {
+        const date = new Date();
+        let err = false
+        const loggArr = []
+        Object.entries(formInput).map((pair) => {
+            loggArr.push(
+            {
+                date: date.toISOString(),
+                requestedBy: "xQIU41mR69s",
+                requestedOf: pair[0],
+                dataElement: commodity,
+                amount: pair[1],
+            });  
+        })
+        log(loggArr, "request").catch((error) => {
+            setAlerts((prev) =>  [...prev, <AlertBar critical children={error.toString()} key={crypto.randomUUID()}/>]);
+            err = true
+        })
+        if (!err) {
+            setAlerts((prev) =>  [...prev, 
+                <AlertBar 
+                    success children={"successfully requested from: " + Object.keys(formInput).reduce((tot, cur) => {
+                        return tot + ", " + orgs.filter((x) => { return x.id === cur })[0].name
+                    }, "").slice(2)} 
+                    key={crypto.randomUUID()}
+                />
+            ]);
+        }
+    }
 }
